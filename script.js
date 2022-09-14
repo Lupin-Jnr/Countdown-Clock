2f719b826838740b9355c2a63434f5830f210657
Lupin-Jnr <onyekonwuchibuzo@gmail.com>
2022-09-13 23:55:03 -0700

    First commit

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ timer= () =>{
     const now = newDate().getTime();
     const diff = finalDate - now;
     if(diff < 0){
-        document.getElementById('container')
+        document.getElementById('container').style.display="None"
+        document.getElementById('if-expired').style.display="block"
     }
+    let days=math.floor(diff/(1000 * 60 * 60 * 24));
+    let hours=math.floor(diff%(1000 * 60 * 60 * 24)/(1000 * 60 * 60));
+    let minutes=math.floor(diff%(1000 * 60 * 60)/(1000 * 60));
+    let seconds=math.floor(diff/(1000 * 60)/1000);
+
+    days <= 9 ? days = `00${days}`:
+    days <= 99 ? days = `0${days}`:
+    hours <= 99 ? days = `0${hours}`:
+    minutes <= 99 ? days = `0${minutes}`:
+    seconds <= 99 ? days = `0${seconds}`:
 }
