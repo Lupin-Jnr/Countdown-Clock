a86d4c2f8b91efbd85bbf7bd22f364f76ef72d44
Lupin-Jnr <onyekonwuchibuzo@gmail.com>
2022-09-14 16:39:05 -0700

    First Commit

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const finalDate = newDate("May 05,2023 00:00:00").getTime();
+const finalDate = new Date ("May 24, 2023 00:00:00").getTime();
 
-timer= () =>{
-    const now = newDate().getTime();
+timer= () => {
+    const now = new Date().getTime();
     const diff = finalDate - now;
-    if(diff < 0){
-        document.getElementById('container').style.display="None"
-        document.getElementById('if-expired').style.display="block"
+    if (diff < 0){
+        clearInterval(x);
+        document.getElementById('container').style.display = "none";
+        document.getElementById('if-expired').style.display = "block";
     }
-    let days=math.floor(diff/(1000 * 60 * 60 * 24));
-    let hours=math.floor(diff%(1000 * 60 * 60 * 24)/(1000 * 60 * 60));
-    let minutes=math.floor(diff%(1000 * 60 * 60)/(1000 * 60));
-    let seconds=math.floor(diff/(1000 * 60)/1000);
+    let days = Math.floor(diff/ (1000*60*60*24));
+    let hours = Math.floor ((diff % (1000*60*60*24))/(1000*60*60));
+    let minutes = Math.floor((diff % (1000*60*60)) / (1000*60));
+    let seconds = Math.floor((diff % (1000*60))/ 1000);
 
-    days <= 9 ? days = `00${days}`:
-    days <= 99 ? days = `0${days}`:
-    hours <= 99 ? days = `0${hours}`:
-    minutes <= 99 ? days = `0${minutes}`:
-    seconds <= 99 ? days = `0${seconds}`:
+    days <=9 ? days = `00${days}` :
+        days <=99 ? days = `0${days}`: days;
+    hours <=9 ? hours = `0${hours}` : hours;
+    minutes <=9 ? minutes = `0${minutes}` : minutes;
+    seconds <=9 ? seconds = `0${seconds}` : seconds;
+   
+
+    document.querySelector("#days .box").textContent = days;
+    document.querySelector("#hours .box").textContent = hours;
+    document.querySelector("#minutes .box").textContent = minutes;
+    document.querySelector("#seconds .box").textContent = seconds;
 }
+
+const x = setInterval(timer, 1000);
